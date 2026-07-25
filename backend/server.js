67e99b8fd6a0f6dd941274366b43c25203d25f95
dfergusbrown <dfergusbrown@gmail.com>
2026-07-25 11:54:45 -0400

    feat(backend): adds server.js--tested locally.

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import router from "./routes/movies.js";
+import cors from "cors";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("YO YO YO wassup!");
+});
+
+// app.use("/movies", router);
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
