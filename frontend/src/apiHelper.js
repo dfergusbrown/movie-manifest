2dcaa54f356f6ab274b09250acffff42c37b96cf
dfergusbrown <dfergusbrown@gmail.com>
2026-07-26 11:41:36 -0400

    feat(frontend): Adds api helper template.

---
+++ frontend/src/apiHelper.js
@@ -0,0 +1,25 @@
+const BASE = import.meta.env.VITE_API_URL;
+
+export async function getAllMovies() {
+  /* get individual movie */
+}
+
+export async function getOneMovie() {
+  /* get ONE movie */
+}
+
+export async function addMovie() {
+  /* add ONE movie */
+}
+
+export async function editMovieData() {
+  /* edit attributes for one Movie */
+}
+
+export async function deleteOneMovie() {
+  /* DELETE one movie */
+}
+
+export async function lookupByUPC() {
+  /* Use the UPCMDB api to lookup movie data */
+}
