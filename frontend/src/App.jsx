25635d29e965e28eaac98313d5fcd5d751d118c9
dfergusbrown <dfergusbrown@gmail.com>
2026-07-28 00:50:24 -0400

    feat(frontend): Adds react router and basic setup in App.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/home";
+import List from "./pages/List";
 
 function App() {
   return (
-    <div className="min-h-dvh max-w-screen flex flex-col items-center justify-center">
-      <section className="">
-        <div id="logo" className="flex flex-col items-center justify-center">
-          <img src="src/assets/movie-reel.png" style={{ height: 100 }} />
-          <h1 className="text-3xl p-3">Movie Manifest</h1>
-        </div>
-        <div className="">
-          <button className="text-3xl p-3 border-4 rounded-xl m-3">LIST</button>
-          <button className="text-3xl p-3 border-4 rounded-xl m-3">
-            ADD MOVIE
-          </button>
-        </div>
-      </section>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/list" element={<List />} />
+    </Routes>
   );
 }
 
