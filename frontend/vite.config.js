a8d9374cbdc10727968ecbe325c60af1eaaf6b71
dfergusbrown <dfergusbrown@gmail.com>
2026-08-02 19:52:58 -0400

    feat(frontend): Corrects vite.config to use loadEnv -- vite tool.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
+import { loadEnv } from "vite";
 
-const TSCALE_MACHINE = import.meta.env.TSCALE_MACHINE;
-const TAILNET_NAME = import.meta.env.TAILNET_NAME;
-const FULL_DNS = `${TSCALE_MACHINE}.${TAILNET_NAME}.ts.net`;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      cert: fs.readFileSync(`./certs/${FULL_DNS}.crt`),
-      key: fs.readFileSync(`./certs/${FULL_DNS}.key`),
+  const TSCALE_MACHINE = env.TSCALE_MACHINE;
+  const TAILNET_NAME = env.TAILNET_NAME;
+  const FULL_DNS = `${TSCALE_MACHINE}.${TAILNET_NAME}.ts.net`;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      https: {
+        cert: fs.readFileSync(`./certs/${FULL_DNS}.crt`),
+        key: fs.readFileSync(`./certs/${FULL_DNS}.key`),
+      },
+      host: true,
     },
-    host: true,
-  },
+  };
 });
