13e762c8564959426a9fae3427899b5767c2de89
dfergusbrown <dfergusbrown@gmail.com>
2026-08-02 19:36:31 -0400

    feat(frontend): Attaches certs that tailnet uses to enable HTTPS (necessary for camera permissions).

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+
+const TSCALE_MACHINE = import.meta.env.TSCALE_MACHINE;
+const TAILNET_NAME = import.meta.env.TAILNET_NAME;
+const FULL_DNS = `${TSCALE_MACHINE}.${TAILNET_NAME}.ts.net`;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    https: {
+      cert: fs.readFileSync(`./certs/${FULL_DNS}.crt`),
+      key: fs.readFileSync(`./certs/${FULL_DNS}.key`),
+    },
+    host: true,
+  },
 });
