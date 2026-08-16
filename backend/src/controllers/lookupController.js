201157a35145ae32e9e540347413329dbbb3e6b7
dfergusbrown <dfergusbrown@gmail.com>
2026-08-16 13:54:16 -0400

    fix(backend): Corrects nested result object.

--- backend/src/controllers/lookupController.js
+++ backend/src/controllers/lookupController.js
@@ -22,7 +22,7 @@ class LookupController {
       if (!result) {
         return res.status(400).send("No result returned from barcode lookup");
       }
-      return res.status(200).json({ result: result });
+      return res.status(200).json(result);
     } catch (error) {
       return res.status(400).json({ errorMessage: error });
     }
